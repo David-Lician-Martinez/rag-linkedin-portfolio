78180d33045e85c0c76ca91406804bc68bdec78b
David-Lician-Martinez <david_crevi99@hotmail.es>
2026-02-27 13:08:51 +0100

    Install react-turnstile + fix TS types

--- web/src/TurnstileGate.tsx
+++ web/src/TurnstileGate.tsx
@@ -1,12 +1,12 @@
 import Turnstile from "react-turnstile";
 
 export function TurnstileGate({ onVerify }: { onVerify: (token: string) => void }) {
-  const siteKey = import.meta.env.VITE_TURNSTILE_SITE_KEY;
+  const siteKey = import.meta.env.VITE_TURNSTILE_SITE_KEY as string;
 
   return (
     <Turnstile
       sitekey={siteKey}
-      onVerify={(token) => onVerify(token)}
+      onVerify={(token: string) => onVerify(token)}
       theme="auto"
     />
   );
